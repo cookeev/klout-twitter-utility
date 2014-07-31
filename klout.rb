95020b15cffdf7b1d5133bf0821cf17e52c8ea70
cookeev <evancooke@gmail.com>
2014-07-31 15:01:14 -0400

    Initial Commit

---
+++ klout.rb
@@ -0,0 +1,21 @@
+require 'rubygems'
+require 'klout'
+require 'pry'
+
+
+Klout.api_key = ''
+
+usernames = File.open('usernames.txt').read
+usernames.each_line do |username|
+	begin
+		username.gsub!(/\n/,"")
+		klout_id = Klout::Identity.find_by_screen_name(username)
+		user = Klout::User.new(klout_id.id)
+		text = "#{user.details["nick"]}, #{user.score["score"]}"
+		text.delete!("\n")
+		File.open('results.txt', 'a') { |file| file.puts text }
+		sleep 0.2
+	rescue
+		binding.pry
+	end
+end
